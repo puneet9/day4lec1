7e34176563611790608f0567801b8da5a923ddad
puneet9 <user1@Administrators-iMac-75.local>
2014-05-08 15:16:02 -0400

    while

---
+++ while/while/main.m
@@ -0,0 +1,25 @@
+//
+//  main.m
+//  while
+//
+//  Created by User1 on 2014-05-08.
+//  Copyright (c) 2014 puneet. All rights reserved.
+//
+
+#import <Foundation/Foundation.h>
+
+int main(int argc, const char * argv[])
+{
+
+    @autoreleasepool {
+        int count = 0;
+    while (count <=9)
+    {
+        NSLog(@"%i",count);
+        ++count;
+    }
+        
+    }
+        return 0;
+}
+
